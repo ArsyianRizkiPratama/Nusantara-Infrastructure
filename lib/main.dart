7ab2cedf6b4731b56d665eab5f2c9937465b3c5e
Arsyian Rizki Pratama <81981819+ArsyianRizkiPratama@users.noreply.github.com>
2023-01-06 00:19:08 +0700

    Mengubah home line 22 jadi LoadingPage

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
-      home: const WidgetTree(),
+      home: const LoadingPage(),
     );
   }
 }
